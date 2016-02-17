b205f134778d79cb0f9a3f9cdc7f532b167d8551
Charles Harrington <charles.harrington@gmail.com>
2016-02-17 15:19:40 -0800

    (styles) add basic styles to Login component

--- expenseTime/src/components/Login.js
+++ expenseTime/src/components/Login.js
@@ -2,14 +2,27 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
 class Login extends Component {
+  
+  signin() {
+    console.log("I'm signing in!!!");
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text> HELLO WORLD </Text>
+        <Text style={styles.logo}> EXPENSE </Text>
+        <Text style={styles.logo}> TIME </Text>
+        <TouchableHighlight
+          style = {styles.button}
+          onPress = {this.signin.bind(this)}
+          underlayColor='#55BCFF'>
+          <Text style={styles.buttonText}>SIGN IN</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -19,7 +32,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#55BCFF'
+  },
+  logo: {
+    fontSize: 50,
+    textAlign: 'center',
+    color: '#DD2524',
+    fontWeight: 'bold',
+  },
+  button: {
+    width: 120,
+    height: 50,
+    borderRadius: 5,
+    borderWidth: 3,
+    borderColor: 'white',
+    backgroundColor: '#00D3A3',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 22,
+    alignSelf: 'center',
   }
 });
 
